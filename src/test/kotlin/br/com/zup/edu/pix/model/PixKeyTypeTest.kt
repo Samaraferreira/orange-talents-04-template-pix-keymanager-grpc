f5c81c76adf5804c862bef6fe32d04cb6a43aad4
Samara Ferreira <ferreirasamara.sf@gmail.com>
2021-06-17 14:10:21 -0300

    fix: corrigindo descrição dos testes

--- src/test/kotlin/br/com/zup/edu/pix/model/PixKeyTypeTest.kt
+++ src/test/kotlin/br/com/zup/edu/pix/model/PixKeyTypeTest.kt
@@ -17,7 +17,7 @@ internal class PixKeyTypeTest {
         }
 
         @Test
-        fun `should not be valid when key value is null or blank`() {
+        fun `should not be valid if key value is not null or blank`() {
             with(PixKeyType.RANDOM_KEY) {
                 assertFalse(isValid("uma chave qualquer"))
             }
@@ -26,8 +26,16 @@ internal class PixKeyTypeTest {
 
     @Nested
     inner class EMAIL {
+
         @Test
-        fun `should be valid when email is null or blank`() {
+        fun `should be valid if email is valid`() {
+            with(PixKeyType.EMAIL) {
+                assertTrue(isValid("test@teste.com"))
+            }
+        }
+
+        @Test
+        fun `should not be valid if email is null or blank`() {
             with(PixKeyType.EMAIL) {
                 assertFalse(isValid(null))
                 assertFalse(isValid(""))
@@ -35,14 +43,7 @@ internal class PixKeyTypeTest {
         }
 
         @Test
-        fun `should be valid when email is valid`() {
-            with(PixKeyType.EMAIL) {
-                assertTrue(isValid("test@teste.com"))
-            }
-        }
-
-        @Test
-        fun `should not be valid when email is invalid`() {
+        fun `should not be valid if email is invalid`() {
             with(PixKeyType.EMAIL) {
                 assertFalse(isValid("test"))
             }
@@ -51,8 +52,16 @@ internal class PixKeyTypeTest {
 
     @Nested
     inner class CPF {
+
         @Test
-        fun `should be valid when cpf is null or blank`() {
+        fun `should be valid if cpf is valid`() {
+            with(PixKeyType.CPF) {
+                assertTrue(isValid("26332985089"))
+            }
+        }
+
+        @Test
+        fun `should not be valid if cpf is null or blank`() {
             with(PixKeyType.CPF) {
                 assertFalse(isValid(null))
                 assertFalse(isValid(""))
@@ -60,14 +69,7 @@ internal class PixKeyTypeTest {
         }
 
         @Test
-        fun `should be valid when cpf is valid`() {
-            with(PixKeyType.CPF) {
-                assertTrue(isValid("26332985089"))
-            }
-        }
-
-        @Test
-        fun `should not be valid when cpf is invalid`() {
+        fun `should not be valid if cpf is invalid`() {
             with(PixKeyType.CPF) {
                 assertFalse(isValid("315.251.220-10"))
             }
@@ -76,8 +78,16 @@ internal class PixKeyTypeTest {
 
     @Nested
     inner class PHONE_NUMBER {
+
         @Test
-        fun `should be valid when phone number is null or blank`() {
+        fun `should be valid if phone number is valid`() {
+            with(PixKeyType.PHONE_NUMBER) {
+                assertTrue(isValid("+5585988714077"))
+            }
+        }
+
+        @Test
+        fun `should not be valid if phone number is null or blank`() {
             with(PixKeyType.PHONE_NUMBER) {
                 assertFalse(isValid(null))
                 assertFalse(isValid(""))
@@ -85,14 +95,7 @@ internal class PixKeyTypeTest {
         }
 
         @Test
-        fun `should be valid when phone number is valid`() {
-            with(PixKeyType.PHONE_NUMBER) {
-                assertTrue(isValid("+5585988714077"))
-            }
-        }
-
-        @Test
-        fun `should not be valid when phone number is invalid`() {
+        fun `should not be valid if phone number is invalid`() {
             with(PixKeyType.PHONE_NUMBER) {
                 assertFalse(isValid("5585988714077"))
             }
